72a4c1d5a8e0f3704b0e4091100df4fb6bfb255b
Carter C <cclements3150@gmail.com>
2020-07-22 23:52:19 -0400

    idk if this works but im gonna try it anyway

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - The sibling of a binary tree node
+ * @node: The node to find the sibling of
+ * 
+ * Return: The node itself or NULL on failure
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *origin = node;
+
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent->right == origin)
+		return (node->parent->left);
+	else if (node->parent->left == origin)
+		return (node->parent->right);
+
+	return (NULL);
+	
+}
